ea289c7f450c03eddf18df9029ea4bab4bfa2b80
SuzyKenne <suzyndatewo@gmail.com>
2024-09-13 10:33:40 +0100

    contact list updated

--- resources/views/layouts/contactList.blade.php
+++ resources/views/layouts/contactList.blade.php
@@ -8,10 +8,12 @@
                 <img class="search-icons" src='icons/ic_baseline-search.png' alt="">
                 <input type="text" placeholder="Search...">
             </div>
-            <select>
-                <option>Sort by: A-Z</option>
-                <option>Sort by: Z-A</option>
-            </select>
+            <form action="{{ url('/') }}" method="GET">
+                <select name="sort" onchange="this.form.submit()">
+                    <option value="asc" {{ $sort == 'asc' ? 'selected' : '' }}>Sort by: A-Z</option>
+                    <option value="desc" {{ $sort == 'desc' ? 'selected' : '' }}>Sort by: Z-A</option>
+                </select>
+            </form>
         </div>   
     </div>            
     <table>
@@ -25,7 +27,12 @@
         <tbody class="table-info">
             @foreach ($contacts as $contact)
                 <tr>
-                    <td><img  src="{{ $contact->image }}" alt="{{ $contact->name }}" class="contact-avatar">{{ $contact->name }}</td>
+                    <td>
+                        <a href="{{ route('contacts.show', $contact->id) }}">
+                           <img  src="{{ asset('images/'. $contact->image )}}" alt="{{ $contact->name }}" class="contact-avatar">
+                         {{ $contact->name }} 
+                        </a>                        
+                    </td>
                     <td>{{ $contact->number }}</td>
                     <td>{{ $contact->email }}</td>
                 </tr> 
