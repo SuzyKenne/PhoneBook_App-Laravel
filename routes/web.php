d8eaa52adb1b6814cea14d7f44a5d802c6a93b5d
SuzyKenne <suzyndatewo@gmail.com>
2024-09-13 10:34:22 +0100

    added  route  to navigate to the create contact file

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CreateController;
+use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContactController::class, 'index']);
+Route::resource('contacts', ContactController::class);
 Route::get('layouts.createContact', [CreateController::class, 'create'])->name('layouts.createContact');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
