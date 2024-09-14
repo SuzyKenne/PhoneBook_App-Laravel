bf6811af44358d2bef7eb566325cfb5c11f8a750
SuzyKenne <suzyndatewo@gmail.com>
2024-09-14 23:11:31 +0100

    added the root url

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
            <img src="{{ asset('icons/Frame 27.png') }}" alt="Add icon"> 
         </a>
         
-        <a href="#" class="nav-item active">
+        <a href="{{ url('/') }}" class="nav-item active">
             <img src="{{ asset('icons/solar_user-bold.png') }}" alt="Contacts icon">
             Contacts
         </a>
